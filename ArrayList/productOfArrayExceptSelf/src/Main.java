722233fc55aad4e8ca1a0768446687d4613b22c3
Colin <choude4@gmail.com>
2023-01-20 15:53:46 -0500

    comments

--- ArrayList/productOfArrayExceptSelf/src/Main.java
+++ ArrayList/productOfArrayExceptSelf/src/Main.java
@@ -7,19 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<Integer>();
-//        for (int i = 1; i < 5; i++) {
-//            arr.add(i);
-//        }
-        arr.add(-1);
-        arr.add(1);
-        arr.add(0);
-        arr.add(-3);
-        arr.add(3);
+        for (int i = 1; i < 5; i++) {
+            arr.add(i);
+        }
+//        arr.add(-1);
+//        arr.add(1);
+//        arr.add(0);
+//        arr.add(-3);
+//        arr.add(3);
         System.out.println(arr);
-        allProductExcept(arr);
+        allProductExceptBetter(arr);
     }
 
-    // this solution works but it is O(N^2) time complexity due to the nested for loop
+    // this solution works, but it is O(N^2) time complexity due to the nested for loop
     public static List<Integer> allProductExcept(List<Integer> x){
         List<Integer> retList = new ArrayList<Integer>();
         for (int i = 0; i < x.size(); i++) {
@@ -40,9 +40,21 @@ public class Main {
         List<Integer> retList = new ArrayList<Integer>();
         // multiply left side
         int start = 1;
-        for (int i = 0; i < x.size(); i++) {
-
+        for (int i = 0; i <x.size() ; i++) {
+            retList.add(start);
         }
+
+        for (int i = 0; i < x.size(); i++) {
+            retList.set(i, start*retList.get(i));
+            start *= x.get(i);
+        }
+
+        start = 1;
+        for (int i = x.size()-1; i >= 0; i--) {
+            retList.set(i, start*retList.get(i));
+            start *= x.get(i);
+        }
+        System.out.println(retList);
 
         return new ArrayList<>();
     }
