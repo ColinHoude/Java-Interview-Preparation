0e1a1e72e281b7e4b34de72615b49848f0cd5355
Colin <choude4@gmail.com>
2023-01-26 14:20:32 -0500

    added Binary Search to Two Sum
    implemented a more efficient solution using binary search
    
    -- I must remember 'sorted array' == binary search

--- TwoPointers/twoSum2/src/Main.java
+++ TwoPointers/twoSum2/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>(Arrays.asList(2,7,11,15));
         twoSum(arr, 9);
+        twoSumBinary(arr, 9);
     }
 
     // brute force method using a nested for loop -- slow
@@ -19,6 +20,27 @@ public class Main {
                     }
                 }
             }
+        }
+        System.out.println(retList);
+    }
+
+    // we can use binary search because we know the array is sorted
+    // Sorted array = use binary search
+    public static void twoSumBinary(List<Integer> x, int target){
+        int i = 0;
+        int j = x.size()-1;
+        List<Integer> retList = new ArrayList<>();
+        while(i < j){
+            if(x.get(i) + x.get(j) == target){
+                retList.add(i+1);
+                retList.add(j+1);
+                break;
+            }
+            else if(x.get(i) + x.get(j) < target){
+                i++;
+            }
+            else
+                j--;
         }
         System.out.println(retList);
     }
