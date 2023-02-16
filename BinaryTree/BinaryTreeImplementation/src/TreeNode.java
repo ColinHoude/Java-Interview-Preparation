38e3c7797b772028cbf43fbf51203d5276c6d6a6
Colin <choude4@gmail.com>
2023-02-16 15:28:11 -0500

    Started implementing Binary Tree
    - Inverse Tree
    - insert Node

--- BinaryTree/BinaryTreeImplementation/src/TreeNode.java
+++ BinaryTree/BinaryTreeImplementation/src/TreeNode.java
@@ -1,17 +1,57 @@
+import javax.swing.*;
+import java.util.*;
+
 public class TreeNode {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // setting up binary tree
+        List<Integer> listToTree = new ArrayList<>();
+        for (int i = 1; i < 11; i++) {
+            listToTree.add(i);
+        }
+
+        System.out.println(listToTree);
     }
 
     int val;
     TreeNode left;
     TreeNode right;
     TreeNode() {};
-    TreeNode(int val){ this.val = val; }
+    TreeNode(int val){ this.val = val;}
     TreeNode(int val, TreeNode left, TreeNode right){
         this.val = val;
         this.right = right;
         this.left = left;
+    }
+
+    public static TreeNode invertTree(TreeNode root){
+        // recursion call until the tree points to null
+        // also need a temp node to store middle while swapping
+        if(root == null) return null;
+        TreeNode Temp = root.left;
+        root.left = root.right;
+        root.right = Temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public static void printTree(TreeNode root){
+        // this is just a basic print, id like to format it somehow
+        if(root == null) return;
+        System.out.println(root.val);
+        printTree(root.left);
+        printTree(root.right);
+    }
+
+    public static void insertNode(TreeNode root, int val){
+        root.val = val;
+    }
+
+    public static TreeNode listToTree(TreeNode root, List<Integer> x){
+        // need to sort list
+        // binary search it to insert
+        Collections.sort(x);
+
     }
 
 
