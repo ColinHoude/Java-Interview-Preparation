93f5d6a773557d8e42f1ba47d1581f2c6f3c2672
Colin <choude4@gmail.com>
2023-02-20 15:45:46 -0500

    adding more functions to the Binary Tree Class
    - Ordered list to binary tree
    - print tree

--- BinaryTree/BinaryTreeImplementation/src/TreeNode.java
+++ BinaryTree/BinaryTreeImplementation/src/TreeNode.java
@@ -5,11 +5,12 @@ public class TreeNode {
     public static void main(String[] args) {
         // setting up binary tree
         List<Integer> listToTree = new ArrayList<>();
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 6; i++) {
             listToTree.add(i);
         }
-
         System.out.println(listToTree);
+        TreeNode root = listToTree(listToTree, 0, listToTree.size()-1);
+        printTree(root);
     }
 
     int val;
@@ -35,23 +36,30 @@ public class TreeNode {
         return root;
     }
 
+    // print tree will print the tree in ascending order if correctly set up
     public static void printTree(TreeNode root){
         // this is just a basic print, id like to format it somehow
         if(root == null) return;
-        System.out.println(root.val);
         printTree(root.left);
+        System.out.print(root.val + " ");
         printTree(root.right);
+
     }
 
     public static void insertNode(TreeNode root, int val){
         root.val = val;
     }
 
-    public static TreeNode listToTree(TreeNode root, List<Integer> x){
+    public static TreeNode listToTree(List<Integer> x, int left, int right){
         // need to sort list
         // binary search it to insert
         Collections.sort(x);
-
+        if(left > right) return null;
+        int mid = left + (right - left) /2;
+        TreeNode root = new TreeNode(x.get(mid));
+        root.left = listToTree(x, left, mid -1);
+        root.right = listToTree(x, mid + 1, right);
+        return root;
     }
 
 
