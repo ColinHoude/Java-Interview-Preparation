ec2f23bf890eea010a8e57206b9c27ac1a15b3f0
Colin <choude4@gmail.com>
2023-03-07 17:24:22 -0500

    Started a Backtracking Directory
    - basic algorithm is a recursion call
    - going to have to study it more

---
+++ Backtracking/powerSets/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+
+// BACKTRACKING //
+
+
+public class Main {
+    static int numOfCalls = 0;
+    public static void main(String[] args) {
+        // Given an integer array nums of unique elements, return all possible subsets (the power set).
+        // The solution set must not contain duplicate subsets. Return the solution in any order.
+        List<List<Integer>> powerSet = new ArrayList<>();
+         int nums[] = {1,2,3};
+         powerSet = subSet(nums);
+         System.out.println(powerSet);
+        System.out.println(numOfCalls);
+    }
+
+    public static List<List<Integer>> subSet( int [] nums){
+        // backtracking algorith
+        List<List<Integer>> retList = new ArrayList<>();
+        backTrack(retList, new ArrayList<>(), nums, 0);
+        return retList;
+    }
+
+    private static void backTrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
+        numOfCalls++;
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            tempList.add(nums[i]);
+            backTrack(list, tempList, nums, i +1);
+            tempList.remove(tempList.size()-1);
+        }
+    }
+}
--- Graphs/NumberofIslands/src/Main.java
+++ Graphs/NumberofIslands/src/Main.java
@@ -34,6 +34,5 @@ public class Main {
         clearGrid(grid, i, j+1);
         clearGrid(grid, i-1, j);
         clearGrid(grid, i, j-1);
-        return;
     }
 }
